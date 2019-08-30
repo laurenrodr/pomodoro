8c8d61295cb2bae8a8cbcecb47a5810f1cc7428d
Lauren Rodriguez <laurenrodriguez729@gmail.comm>
2019-08-30 14:23:04 -0400

    add default time variables

--- app.js
+++ app.js
@@ -3,21 +3,27 @@ var minute = second * 60;
 var hour = minute * 60;
 var end;
 var id;
-var time = "00:00";
+const pomo_min = "00"
+// const pomo_min = document.getElementById("minutes");
+const pomo_sec = "14";
+var time = `${pomo_min}:${pomo_sec}`;
+var isResume = false;
+var count = document.getElementById("timer");
+count.textContent = time;
 
 function displayTimer() {
+    console.log("time = " + time);
     var now = Date.now();
     var distance = end - now;
 
     if (distance < 0) {
         clearInterval(id);
-        // expiry code goes here
     }
 
     var minutes = Math.floor((distance % hour) / minute);
     var seconds = Math.floor((distance % minute) / second);
     var milliseconds = distance % second;
-    var count = document.getElementById('timer');
+
 
     count.textContent = `${minutes}:${seconds < 10 ? '0' : ''}${seconds}`;
 }
@@ -25,22 +31,38 @@ function displayTimer() {
 function startTimer(m, s) {
     end = Date.now() + (m * 60 * 1000) +  (s * 1000);
     id = setInterval(displayTimer, 10);
+    document.getElementById("pause").style.display = "inline";
+    document.getElementById("start").style.display = "none";
 }
 
 function pauseTimer() {
     time = timer.textContent;
     clearInterval(id);
+    document.getElementById("resume").style.display = "inline";
+    document.getElementById("pause").style.display = "none";
+    isResume = true;
 }
 
 function resumeTimer() {
-    var t = time.split(":");
-    startTimer(parseInt(t[0], 10), parseInt(t[1], 10));
+        var t = time.split(":");
+        startTimer(parseInt(t[0], 10), parseInt(t[1], 10));
+    document.getElementById("resume").style.display = "none";
+    document.getElementById("pause").style.display = "inline";
+
+}
+
+function stopTimer() {
+    clearInterval(id);
+}
+
+function resetTimer() {
+    startTimer();
 }
 
 start.addEventListener("click", function () {
-    startTimer(0, 25);
+    startTimer(pomo_min, pomo_sec);
 }, false);
 
-pause.addEventListener("click", pauseTimer, false);
-
-resume.addEventListener("click", resumeTimer, false);
+// pause.addEventListener("click", pauseTimer, false);
+//
+// resume.addEventListener("click", resumeTimer, false);
